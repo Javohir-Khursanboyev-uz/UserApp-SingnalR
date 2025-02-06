cd679214946e30f87348a3a67c6967a225fbd630
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-07 01:04:33 +0500

    Add Enviroment, File, HttpContext, PasswordHasher Helpers

---
+++ src/UserApp-SingnalR.Service/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace UserApp_SingnalR.Service.Helpers;
+
+public static class EnvironmentHelper
+{
+    public static string WebRootPath { get; set; }
+    public static string EmailAddress { get; set; }
+    public static string EmailPassword { get; set; }
+    public static string SmtpHost { get; set; }
+    public static string SmtpPort { get; set; }
+}
---
+++ src/UserApp-SingnalR.Service/Helpers/FileHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using UserApp_SingnalR.Shared.DTOs.Assets;
+
+namespace UserApp_SingnalR.Service.Helpers;
+
+public static class FileHelper
+{
+    public static async Task<(string Path, string Name)> CreateFileAsync(AssetCreateModel asset)
+    {
+        var directoryPath = Path.Combine(EnvironmentHelper.WebRootPath, "Assets", asset.FileType.ToString());
+        if (!Directory.Exists(directoryPath))
+            Directory.CreateDirectory(directoryPath);
+
+        var fileName = MakeFileName(asset.File.FileName);
+        var fullPath = Path.Combine(directoryPath, fileName);
+
+        var stream = File.Create(fullPath);
+        await asset.File.CopyToAsync(stream);
+        stream.Close();
+
+        return (fullPath, fileName);
+    }
+
+    private static string MakeFileName(string fileName)
+    {
+        string fileExtension = Path.GetExtension(fileName);
+        string guid = Guid.NewGuid().ToString();
+        return $"{guid}{fileExtension}";
+    }
+}
