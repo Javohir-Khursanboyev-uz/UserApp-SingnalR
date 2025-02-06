651eac3d542b2def5e26e392a924800cc6fb42e6
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-06 18:19:15 +0500

    Add Entities And Enums

---
+++ src/UserApp-SingnalR.Domain/Entities/Contact.cs
@@ -0,0 +1,13 @@
+﻿using UserApp_SingnalR.Domain.Commons;
+
+namespace UserApp_SingnalR.Domain.Entities;
+
+public class Contact : Auditable
+{
+    public long OwnerId { get; set; }
+    public User Owner { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public string AliasName { get; set; }
+    public IEnumerable<Message> Messages { get; set; }
+}
---
+++ src/UserApp-SingnalR.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using UserApp_SingnalR.Domain.Commons;
+
+namespace UserApp_SingnalR.Domain.Entities;
+
+public class User : Auditable
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public long? PictureId { get; set; }
+    public Asset Picture { get; set; }
+    public long RoleId { get; set; }
+    public UserRole Role { get; set; }
+    public IEnumerable<Contact> Contacts { get; set; }
+}
