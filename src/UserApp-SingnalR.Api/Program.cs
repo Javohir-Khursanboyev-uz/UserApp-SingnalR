8de2e4ef4a9b7f3fd830b442b7bc48ef2302fcaf
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-07 15:02:09 +0500

    Setup Startup Project

--- src/UserApp-SingnalR.Api/Program.cs
+++ src/UserApp-SingnalR.Api/Program.cs
@@ -1,6 +1,9 @@
-using System;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using UserApp_SingnalR.Api.Helpers;
 using UserApp_SingnalR.DataAcces.DbContexts;
+using UserApp_SingnalR.Service.Mappers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +12,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers(options =>
+    options.Conventions.Add(new RouteTokenTransformerConvention(new ConfigurationApiUrlName())));
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddExceptionHandlers();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddValidators();
+builder.Services.AddServices();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.InjectEnvironmentItems();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
