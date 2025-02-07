3130ce89f087fde09915fc86247efea95a486ea2
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-07 11:03:40 +0500

    Add UserService Create Method and Validation

--- src/UserApp-SingnalR.Service/Services/Users/UserService.cs
+++ src/UserApp-SingnalR.Service/Services/Users/UserService.cs
@@ -1,6 +1,33 @@
-﻿namespace UserApp_SingnalR.Service.Services.Users;
+﻿using AutoMapper;
+using UserApp_SingnalR.DataAcces.UnitOfWorks;
+using UserApp_SingnalR.Domain.Entities;
+using UserApp_SingnalR.Service.Configurations;
+using UserApp_SingnalR.Service.Extensions;
+using UserApp_SingnalR.Service.Helpers;
+using UserApp_SingnalR.Service.Validators.Users;
+using UserApp_SingnalR.Shared.DTOs.Users;
+using UserApp_SingnalR.Shared.Exceptions;
 
-public class UserService
+namespace UserApp_SingnalR.Service.Services.Users;
+
+public class UserService(IMapper mapper, IUnitOfWork unitOfWork, UserCreateModelValidator createModelValidator) : IUserService
 {
+    public async Task<UserViewModel> CreateAsync(UserCreateModel createModel)
+    {
+        await createModelValidator.EnsureValidatedAsync(createModel);
 
+        var existUser = await unitOfWork.Users.SelectAsync(u => u.Email == createModel.Email || u.UserName == createModel.UserName);
+        if (existUser is not null)
+            throw new AlreadyExistException($"User already exist this email {createModel.Email} or username {createModel.UserName}");
+
+        var user = mapper.Map<User>(createModel);
+        user.Password = PasswordHasher.Hash(createModel.Password);
+        user.RoleId = Constants.UserRoleId;
+        user.PictureId = Constants.DefultImageId;
+
+        var createdUser = await unitOfWork.Users.InsertAsync(user);
+        await unitOfWork.SaveAsync();
+
+        return mapper.Map<UserViewModel>(createdUser);
+    }
 }
